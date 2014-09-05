806a8c4d1fd2cb7b1c839bd86736f5c34b89e3e9
Peta Tomanek <petriczekk@gmail.com>
2014-09-05 11:42:36 +0200

    Remove duplicated directory

--- src/issuetracking/org/service/iface/GenericServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package issuetracking.org.service.iface;
-import java.util.List;
-
-public interface GenericServiceInterface<E> {
-    public boolean create(E obj) throws Exception;
-    public E find(int id) throws Exception;
-    public List<E> findAll() throws Exception;
-    public boolean update(E obj) throws Exception;
-}
